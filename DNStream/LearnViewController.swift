d2128644130955ae89f736af10249b8ea8948774
Chris Scott <cyberstrike@gmail.com>
2015-10-14 21:55:42 -0400

    added webview and constraints to it and LearniOS

---
+++ DNStream/LearnViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LearnViewController.swift
+//  DNStream
+//
+//  Created by Chris Scott on 10/14/15.
+//  Copyright © 2015 Chris Scott. All rights reserved.
+//
+
+import UIKit
+
+class LearnViewController: UIViewController {
+
+    @IBOutlet weak var dialogView: DesignableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
